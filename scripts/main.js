6722518fc96144cb557d2c90b94119ecd9e9a075
marstilen <tilen.mars@proton.me>
2025-10-27 17:01:17 +0100

    add "playRound" function

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,13 @@
 const choices = ["rock", "paper", "scissors"];
+let computerScore = 0; 
+let humanScore = 0; 
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
 function getComputerChoice(){
-    let randomChoice = getRandomInt(3);
-    return choices[randomChoice];
+    return getRandomInt(3);
 }
 
 function getHumanChoice() {
@@ -14,3 +15,21 @@ function getHumanChoice() {
     userChoice = userChoice.toLowerCase();
     return userChoice;
 }
+
+function playRound(computerChoice, humanChoice) {
+    humanChoice = choices.indexOf(humanChoice);
+    if (computerChoice == humanChoice) {
+        console.log("Tie!");
+    }
+    else if(humanChoice-1 == computerChoice || (humanChoice == 0 && computerChoice == 2)){
+        console.log(`You won, ${choices[humanChoice]} beats ${choices[computerChoice]}!`);
+    }
+    else if(humanChoice < computerChoice || (computerChoice == 0 && humanChoice == 2))
+    {
+        console.log(`You lost, ${choices[computerChoice]} beats ${choices[humanChoice]}!`);
+    }
+    else{
+        console.log("error")
+    }
+}
+playRound(getComputerChoice(), getHumanChoice());
