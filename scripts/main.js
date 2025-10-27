9aaec8c175a39e478ccacfdd6ffc19f6d07bcbc7
marstilen <tilen.mars@proton.me>
2025-10-27 17:29:18 +0100

    implement playGame logic

--- scripts/main.js
+++ scripts/main.js
@@ -23,13 +23,25 @@ function playRound(computerChoice, humanChoice) {
     }
     else if(humanChoice-1 == computerChoice || (humanChoice == 0 && computerChoice == 2)){
         console.log(`You won, ${choices[humanChoice]} beats ${choices[computerChoice]}!`);
+        humanScore++;
     }
     else if(humanChoice < computerChoice || (computerChoice == 0 && humanChoice == 2))
     {
         console.log(`You lost, ${choices[computerChoice]} beats ${choices[humanChoice]}!`);
+        computerScore++;
     }
     else{
         console.log("error")
     }
 }
-playRound(getComputerChoice(), getHumanChoice());
+
+function playGame(pointsToWin) {
+  while(humanScore < pointsToWin && computerScore < pointsToWin)
+    {
+        playRound(getComputerChoice(), getHumanChoice());
+        console.log(`Player: ${humanScore} computer: ${computerScore}`)
+    }  
+    humanScore = 0;
+    computerScore = 0; 
+}
+
